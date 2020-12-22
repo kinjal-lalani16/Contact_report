d71a7095ba91f77cb44b2bf79ca713ebbab09539
Kinal <kinjal.lalani@icreativetechnolabs.com>
2020-12-22 10:32:29 +0530

    [Init]Initital commit

---
+++ contacts_report/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "contact_report",
+    'summary': """This Module will create report of selected contacts.""",
+    'description': """This Module will create report of selected contacts.""",
+    'author': "Aktiv Software",
+    'website': "http://www.aktivsoftware.com",
+    'category': 'Uncategorized',
+    'version': '14.0.1.0.0',
+    'depends': ['base'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+}
